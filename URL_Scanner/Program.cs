2e641ca9b2144b02e18d6baf989f23fe9700b7c4
navee <navee@NAVEEN>
2025-04-07 18:38:11 +0530

    Add project files.

---
+++ URL_Scanner/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program()
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the url :");
+        string url = Console.ReadLine();
+        bool isSuspicouis = CheckForPhishing(url);
+        if (isSuspicouis)
+        {
+            Console.WriteLine("Warning: This URL looks suspicious!");
+        }
+        else
+        {
+            Console.WriteLine("This URL seems okay based on basic checks.");
+        }
+
+    }
+
+    public static bool CheckForPhishing(string url)
+    {
+        if (url.Length > 75)
+        {
+            return true;
+
+        }
+
+        if (url.Contains("a") || url.Contains("//") || url.Contains("http://http"))
+        {
+            return true;
+        }
+        if (!url.Contains(".") || url.EndsWith(".exe"))
+        {
+            return true;
+        }
+        return false;
+    }
+}
